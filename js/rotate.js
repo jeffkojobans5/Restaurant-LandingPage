83f5937bc7f5f2cb507508bff98d718a6a6711cf
jeffkojobans5 <67514352+jeffkojobans5@users.noreply.github.com>
2021-06-22 08:37:49 +0100

    final

--- js/rotate.js
+++ js/rotate.js
@@ -2,9 +2,9 @@
 			// console.log(roundChildren.children)
 
 			let test1 = 'Text 1 I really had fun at the place. Their food tasted great. I would like to visit the place one more time with my family and friends. The waitress was very professional towards me and treated my order with punctuality. <br/><br/> <span id="name"> - Kojo </span>'
-			let test2 = 'Text 1 I really had fun at the place. Their food tasted great. I would like to visit the place one more time with my family and friends. The waitress was very professional towards me and treated my order with punctuality. <br/><br/> <span id="name"> - Akua </span>'
-			let test3 = 'Text 1 I really had fun at the place. Their food tasted great. I would like to visit the place one more time with my family and friends. The waitress was very professional towards me and treated my order with punctuality. <br/><br/> <span id="name"> - Adwoa </span>'
-			let test4 = 'Text 1 I really had fun at the place. Their food tasted great. I would like to visit the place one more time with my family and friends. The waitress was very professional towards me and treated my order with punctuality. <br/><br/> <span id="name"> - Ernest </span>'
+			let test2 = 'Text 2 I really had fun at the place. Their food tasted great. I would like to visit the place one more time with my family and friends. The waitress was very professional towards me and treated my order with punctuality. <br/><br/> <span id="name"> - Ernest </span>'
+			let test3 = 'Text 3 I really had fun at the place. Their food tasted great. I would like to visit the place one more time with my family and friends. The waitress was very professional towards me and treated my order with punctuality. <br/><br/> <span id="name"> - Adwoa </span>'
+			let test4 = 'Text 4 I really had fun at the place. Their food tasted great. I would like to visit the place one more time with my family and friends. The waitress was very professional towards me and treated my order with punctuality. <br/><br/> <span id="name"> - Akua  </span>'
 			
 
 
